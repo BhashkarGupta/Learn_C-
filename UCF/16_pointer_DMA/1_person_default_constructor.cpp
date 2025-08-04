aa4eeab0f734f4a8ff45b6d7e5786b958f8e4138
Bhahskar Gupta <bhashkar.spi@gmail.com>
2025-08-04 21:26:28 +0530

    .

---
+++ UCF/16_pointer_DMA/1_person_default_constructor.cpp
@@ -0,0 +1,3 @@
+// Define a class Person with name and age as instance variables. Provide
+// parameterised constructor with two formal arguments name & age, to initialise
+// instance variables.
---
+++ UCF/16_pointer_DMA/2_complex.cpp
@@ -0,0 +1,6 @@
+// Define a class Complex with instance variables a and b to store real and imaginar
+// part of a complex number. Provide setData) method with formal arguments with th
+// name a and b, to set the values of instance variables. Also define showDatal)
+// method to display instance member variable values.
+// In question-2, write a non member function in which instantiate Complex class
+// dynamically. Initialise instance variables and display their values.
