9efac654a56d3548ce147f338e20deec96eed9f9
Bhahskar Gupta <bhashkar.spi@gmail.com>
2025-08-05 11:32:36 +0530

    completed 16

--- UCF/16_pointer_DMA/1_person_default_constructor.cpp
+++ UCF/16_pointer_DMA/1_person_default_constructor.cpp
@@ -1,3 +1,35 @@
 // Define a class Person with name and age as instance variables. Provide
 // parameterised constructor with two formal arguments name & age, to initialise
 // instance variables.
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Person{
+    private:
+    int age;
+    string name;
+
+    public:
+    Person(int age, string name):age(age), name(name){}
+
+    friend istream& operator>>(istream& in, Person& perObj);
+    friend ostream& operator<<(ostream& out, const Person& perObj);
+};
+
+istream& operator>>(istream& in, Person& perObj){
+    in >> perObj.age >> perObj.name;
+    return in;
+}
+
+ostream& operator<<(ostream& out, const Person& perObj){
+    out << "Name : " << perObj.name << "\nAge : " << perObj.age << endl;
+    return out;
+}
+
+int main(){
+    Person p1(22, "Mohan Kumar");
+    cout << p1;
+    return 0;
+}
--- UCF/16_pointer_DMA/2_complex.cpp
+++ UCF/16_pointer_DMA/2_complex.cpp
@@ -1,6 +1,50 @@
 // Define a class Complex with instance variables a and b to store real and imaginar
-// part of a complex number. Provide setData) method with formal arguments with th
-// name a and b, to set the values of instance variables. Also define showDatal)
+// part of a complex number. Provide setData() method with formal arguments with th
+// name a and b, to set the values of instance variables. Also define showDatal()
 // method to display instance member variable values.
 // In question-2, write a non member function in which instantiate Complex class
 // dynamically. Initialise instance variables and display their values.
+
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+private:
+    int a, b;
+
+public:
+    void setData(int a, int b)
+    {
+        this->a = a;
+        this->b = b;
+    }
+
+    void showData()
+    {
+        if (b < 0)
+        {
+            cout << a << " - i" << -b << endl;
+        }
+        else
+        {
+            cout << a << " + i" << b << endl;
+        }
+    }
+};
+
+void classInitiater(int a, int b)
+{
+    Complex c1;
+    c1.setData(a, b);
+    c1.showData();
+}
+
+int main()
+{
+    int a, b;
+    cout << "Please enter value for complex number\n";
+    cin >> a >> b;
+    classInitiater(a, b);
+    return 0;
+}
