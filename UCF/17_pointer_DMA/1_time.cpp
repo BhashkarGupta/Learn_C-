f8f4ce3b914c331032ce1e142f7b2ea9fe12d573
Bhahskar Gupta <bhashkar.spi@gmail.com>
2025-08-07 10:41:40 +0530

    completed 17

--- UCF/17_pointer_DMA/1_time.cpp
+++ UCF/17_pointer_DMA/1_time.cpp
@@ -6,3 +6,168 @@
 // Define a method to sort Time object array.
 // Define a method to display Time object array values. Array is received
 // through argument.
+
+#include <iostream>
+using namespace std;
+
+class Time
+{
+private:
+    int hr, min, sec;
+    void normalize()
+    {
+        if (sec / 60 > 0)
+        {
+            min += sec / 60;
+            sec = sec % 60;
+        }
+        if (min / 60 > 0)
+        {
+            hr += min / 60;
+            min = min % 60;
+        }
+    }
+
+public:
+    void setTime(int hr, int min, int sec)
+    {
+        this->hr = hr;
+        this->min = min;
+        this->sec = sec;
+        normalize();
+    }
+
+    void showTime()
+    {
+        cout << hr << ":" << min << ":" << sec << endl;
+    }
+
+    Time operator=(const Time &t)
+    {
+        hr = t.hr;
+        min = t.min;
+        sec = t.sec;
+        return *this;
+    }
+
+    bool operator>(const Time &temp)
+    {
+        if (hr > temp.hr)
+        {
+            return true;
+        }
+        else if (hr == temp.hr)
+        {
+            if (min > temp.min)
+            {
+                return true;
+            }
+            else if (min == temp.min)
+            {
+                if (sec > temp.sec)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    friend ostream &operator<<(ostream &out, const Time &temp);
+    friend istream &operator>>(istream &in, Time &temp);
+};
+
+ostream &operator<<(ostream &out, const Time &temp)
+{
+    out << temp.hr << ":" << temp.min << ":" << temp.sec;
+    return out;
+}
+
+istream &operator>>(istream &in, Time &temp)
+{
+    in >> temp.hr >> temp.min >> temp.sec;
+    temp.normalize();
+    return in;
+}
+
+class TimeArray
+{
+private:
+    Time *T = nullptr;
+    int size;
+
+public:
+    TimeArray(int size) : size(size)
+    {
+        if (size > 0)
+        {
+            T = new Time[size];
+            for (int i = 0; i < size; i++)
+            {
+                Time temp;
+                cout << "Please enter time for " << i << " place\n";
+                cin >> temp;
+                *(T + i) = temp;
+            }
+        }
+    }
+
+    TimeArray(int size, Time array[]) : size(size)
+    {
+        if (size > 0)
+        {
+            T = new Time[size];
+            for (int i = 0; i < size; i++)
+            {
+                *(T + i) = array[i];
+            }
+        }
+    }
+
+    TimeArray &sort()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            int temp = i;
+            for (int j = i + 1; j < size; j++)
+            {
+                if (*(T + j) > *(T + temp))
+                {
+                    temp = j;
+                }
+            }
+            if (i != temp)
+            {
+                Time swap = *(T + temp);
+                *(T + temp) = *(T + i);
+                *(T + i) = swap;
+            }
+        }
+        return *this;
+    }
+
+    friend ostream &operator<<(ostream &out, const TimeArray &tArray);
+};
+
+ostream &operator<<(ostream &out, const TimeArray &tArray)
+{
+    out << "[ ";
+    for (int i = 0; i < tArray.size - 1; i++)
+    {
+        out << *(tArray.T + i) << ", ";
+    }
+    out << *(tArray.T + tArray.size - 1) << " ]\n";
+    return out;
+}
+
+int main()
+{
+    cout << "Please Enter the size of the array: \n";
+    int size;
+    cin >> size;
+    TimeArray T_Array(size);
+    cout << T_Array;
+    T_Array.sort();
+    cout << T_Array;
+    return 0;
+}
--- UCF/17_pointer_DMA/5_string.cpp
+++ UCF/17_pointer_DMA/5_string.cpp
@@ -4,3 +4,154 @@
 // input string.
 // define methods to convert string into uppercase, lowercase.
 // define methods to print string, return string, return length of string.
+
+#include <iostream>
+using namespace std;
+
+class String
+{
+private:
+    int length = 0;
+    char *P = nullptr;
+
+public:
+    String() {}
+    String(const char ch[])
+    {
+        int counter = 0;
+        while (ch[counter] != 0)
+        {
+            counter++;
+        }
+        length = counter;
+        P = new char[length + 1];
+        for (int i = 0; i < length; i++)
+        {
+            *(P + i) = ch[i];
+        }
+        *(P + length) = '\0';
+    }
+
+    String(const String &copyStr)
+    {
+        length = copyStr.length;
+        P = new char[length + 1];
+        for (int i = 0; i < length; i++)
+        {
+            *(P + i) = *(copyStr.P + i);
+        }
+        *(P + length) = '\0';
+    }
+
+    String &operator=(const String &copyStr)
+    {
+        if (this == &copyStr) return *this;
+        length = copyStr.length;
+        if (P != nullptr)
+        {
+            delete[] P;
+        }
+        P = new char[length + 1];
+        for (int i = 0; i < length; i++)
+        {
+            *(P + i) = *(copyStr.P + i);
+        }
+        *(P + length) = '\0';
+        return *this;
+    }
+
+    String &operator=(const char copyStr[])
+    {
+        length = 0;
+        while (copyStr[length] != 0)
+        {
+            length++;
+        }
+        if (P != nullptr)
+        {
+            delete[] P;
+        }
+        P = new char[length + 1];
+        for (int i = 0; i < length; i++)
+        {
+            *(P + i) = copyStr[i];
+        }
+        *(P + length) = '\0';
+        return *this;
+    }
+
+    String toUpperCase()
+    {
+        String temp;
+        temp.length = length;
+        temp.P = new char[temp.length + 1];
+        for (int i = 0; i < length; i++)
+        {
+            if (*(P + i) >= 'a' && *(P + i) <= 'z')
+            {
+                *(temp.P + i) = *(P + i) - 32;
+            }
+            else
+            {
+                *(temp.P + i) = *(P + i);
+            }
+        }
+        *(temp.P + temp.length) = '\0';
+        return temp;
+    }
+
+    String toLowerCase()
+    {
+        String temp;
+        temp.length = length;
+        temp.P = new char[temp.length];
+        for (int i = 0; i < length; i++)
+        {
+            if (*(P + i) >= 'A' && *(P + i) <= 'Z')
+            {
+                *(temp.P + i) = *(P + i) + 32;
+            }
+            else
+            {
+                *(temp.P + i) = *(P + i);
+            }
+        }
+        *(temp.P + temp.length) = '\0';
+        return temp;
+    }
+
+    int size()
+    {
+        return length;
+    }
+
+    ~String()
+    {
+        delete[] P;
+    }
+
+    friend ostream &operator<<(ostream &out, const String &str);
+};
+
+ostream &operator<<(ostream &out, const String &str)
+{
+    for (int i = 0; i < str.length; i++)
+    {
+        out << *(str.P + i);
+    }
+    return out;
+}
+
+int main()
+{
+    char ch[] = "This is a string";
+    // String testString1(ch);
+    String testString1;
+    testString1 = ch;
+    cout << testString1 << endl;
+    String testString2 = "This is string 2";
+    cout << testString2.toUpperCase() << endl;
+    cout << testString2.toLowerCase() << endl;
+    cout << testString1.size() << endl;
+    return 0;
+}
